9bee9c2cdc071d6afef0b829ad2147133bfc0364
kianilannoye <kiani_lannoye@hotmail.com>
2016-03-01 19:33:42 +0100

    correcte imports

--- Chronic/src/be/ugent/dao/VersionDao.java
+++ Chronic/src/be/ugent/dao/VersionDao.java
@@ -2,7 +2,13 @@ package be.ugent.dao;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
 import be.ugent.MongoDBSingleton;
 import be.ugent.entitity.Drug;
 import be.ugent.entitity.Version;
